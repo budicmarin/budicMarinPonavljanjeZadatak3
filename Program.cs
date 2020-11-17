cb3ee1231b582c5668fd8faf7cd1cbc1b3722c9f
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-17 09:47:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavljanjeZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] niz=new string[3];
+            Console.WriteLine("Upiši tri stringa");
+            for (int i=0;i<3;i++)
+            {
+                niz[i] = Console.ReadLine();
+            }
+            Array.Sort(niz);
+            Array.Reverse(niz);
+            foreach (string recenica in niz)
+            {
+                Console.WriteLine(recenica);
+            }
+            Console.ReadKey();
+        }
+    }
+}
